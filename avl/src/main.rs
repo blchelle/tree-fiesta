f2cf562a56610345af82f641a90439d2082f38b9
mingyaang <mmao@ualberta.ca>
2021-03-20 21:35:53 -0700

    delete

--- avl/src/main.rs
+++ avl/src/main.rs
@@ -43,6 +43,28 @@ impl<T: Ord+Copy> AVLNode<T> {
         }
     }
 
+    fn getMinVal(node: AVLTree<T>) -> T {
+        let n_unwrap = node.unwrap();
+        let n_bor = n_unwrap.borrow();
+        if n_bor.right.is_some() {
+            return Self::getMinVal(n_bor.right.clone());
+        } else {
+            return n_bor.key;
+        }
+    }
+
+    fn getBalance(node: AVLTree<T>) -> i32 {
+        match node {
+            None => {
+                0
+            }
+            Some(n) => {
+                let n_b = n.borrow();
+                Self::height(n_b.left.clone()) - Self::height(n_b.right.clone())
+            }
+        }
+    }
+
     // insert treating self as the root
     fn insert(node: AVLTree<T>, data : T) -> AVLTree<T> {
         println!("Insertion called..");
@@ -55,14 +77,12 @@ impl<T: Ord+Copy> AVLNode<T> {
         let rc_node = node.clone().unwrap();
         let bal;
         let rcase : RotationCase;
-        {  // mutable block for changing height
+        {  // mutable block for changing height + bst insert
             let mut cur = rc_node.borrow_mut();
 
             if data < cur.key {
-                println!("-> l");
                 cur.left = Self::insert(cur.left.clone(), data);
             } else if data > cur.key {
-                println!("-> r");
                 cur.right = Self::insert(cur.right.clone(), data);
             } else {
                 return node;
@@ -164,29 +184,140 @@ impl<T: Ord+Copy> AVLNode<T> {
         cur.right = rl;
 
         // update height
-        println!("updating heights");
         cur.height = 1 + Self::height(cur.left.clone()).max(Self::height(cur.right.clone()));
-        println!("updating heights");
         r_mut.height = 1 + cur.height.max(Self::height(r_mut.right.clone()));
 
         return r;
     }
+
+    fn delete(root: AVLTree<T>, data : T) -> AVLTree<T> {
+        println!("Delete called");
+        if root.is_none() {
+            return root
+        }
+
+        let rc_node = root.clone().unwrap();
+        let rcase : RotationCase;
+        // delete -
+        let mut rem_node: AVLTree<T> = None;
+        // let mut rem_mut;
+        {
+            let mut cur = rc_node.borrow_mut();
+            let rem_unwrap;
+            let mut rem_mut;
+
+            if data < cur.key {
+                println!("cond1");
+                cur.left = Self::delete(cur.left.clone(), data);
+                rem_node = root;
+                rem_mut = cur;
+            } else if data > cur.key {
+                println!("cond 2");
+                cur.right = Self::delete(cur.right.clone(), data);
+                rem_node = root;
+                rem_mut = cur;
+            } else { // delete here
+                println!("found");
+                if cur.left.is_none() && cur.right.is_none() { // no child
+                    println!("is none");
+                    return None;
+                } else if cur.left.is_some() && cur.right.is_some() {
+                    println!("has some");
+                    // get inorder, copy keys, delete lowest
+                    let tmp = Self::getMinVal(cur.right.clone());
+                    cur.key = tmp;
+                    cur.right = Self::delete(cur.right.clone(), tmp);
+                    rem_node = root.clone();
+                } else if cur.left.is_some() {
+                    rem_node = cur.left.clone();
+                } else if cur.right.is_some() {
+                    rem_node = cur.right.clone();
+                }
+
+                rem_unwrap = rem_node.clone().unwrap();
+                rem_mut = rem_unwrap.borrow_mut();
+            }
+
+            println!("chillin");
+            // let rem_unwrap = rem_node.clone().unwrap();
+            // let mut rem_mut = rem_unwrap.borrow_mut();
+            rem_mut.height = 1 + Self::height(rem_mut.left.clone()).max(Self::height(rem_mut.right.clone()));
+
+            let bal = Self::height(rem_mut.left.clone()) - Self::height(rem_mut.right.clone());
+            let rbal = Self::getBalance(rem_mut.right.clone());
+            let lbal = Self::getBalance(rem_mut.left.clone());
+
+            println!("balance {}", bal);
+
+            if (bal > 1) && (lbal >= 0) {
+                rcase = RotationCase::LL;
+            } else if (bal < -1) && (rbal <= 0) {
+                rcase = RotationCase::RR;
+            } else if (bal > 1) && (lbal < 0) {
+                rcase = RotationCase::LR;
+            } else if (bal < -1) && (rbal > 0) {
+                rcase = RotationCase::RL;
+            } else {
+                rcase = RotationCase::Nil;
+            }
+        }
+
+        if rem_node.is_none() {
+            return rem_node;
+        }
+
+        let rem_unwrap = rem_node.clone().unwrap();
+
+        println!("rcase {:?}", rcase);
+        return match rcase {
+            RotationCase::LL => {
+                Self::rightRotate(rem_unwrap)
+            }
+            RotationCase::RR => {
+                Self::leftRotate(rem_unwrap)
+            }
+            RotationCase::LR => {
+                let tmp = Self::leftRotate(rem_unwrap.borrow().left.clone().unwrap());
+                rem_unwrap.borrow_mut().left = tmp;
+                Self::rightRotate(rem_unwrap)
+            }
+            RotationCase::RL => {
+                let tmp = Self::leftRotate(rem_unwrap.borrow().right.clone().unwrap());
+                rem_unwrap.borrow_mut().right = tmp;
+                Self::leftRotate(rem_unwrap)
+            }
+            RotationCase::Nil => {
+                rem_node
+            }
+        }
+    }
+
+
 }
 
 
 fn main() {
     println!("Hello, world!");
-    let root = AVLNode::new(1);
-    AVLNode::insert(root.clone(), 2);
-    println!("added 2");
-    AVLNode::insert(root.clone(), 3);
-    println!("added 3");
-    AVLNode::insert(root.clone(), 4);
-    AVLNode::insert(root.clone(), 5);
-    AVLNode::insert(root.clone(), 6);
-    AVLNode::insert(root.clone(), 7);
-    AVLNode::insert(root.clone(), 8);
+    let mut root = AVLNode::new(1);
+    root = AVLNode::insert(root.clone(), 2);
+    root = AVLNode::insert(root.clone(), 3);
+    root = AVLNode::insert(root.clone(), 4);
+    root = AVLNode::insert(root.clone(), 5);
+    root = AVLNode::insert(root.clone(), 6);
+    root = AVLNode::insert(root.clone(), 7);
+    root = AVLNode::insert(root.clone(), 8);
+    root = AVLNode::delete(root.clone(), 8);
+    root = AVLNode::delete(root.clone(), 7);
+    root = AVLNode::delete(root.clone(), 6);
+    root = AVLNode::delete(root.clone(), 5);
+    root = AVLNode::delete(root.clone(), 4);
+    root = AVLNode::delete(root.clone(), 3);
+    root = AVLNode::delete(root.clone(), 2);
+    root = AVLNode::delete(root.clone(), 1);
 
+    if root.is_none() {
+        println!("nice");
+    }
 
     // AVLNode::insert(root, 1);
 }
